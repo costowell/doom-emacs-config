671c9bf92385452e09176848fb6c1fa6b7d7e194
Cole Stowell <cole@slide.tech>
2026-01-21 20:05:30 -0500

    feat: enable yaml

--- init.el
+++ init.el
@@ -178,7 +178,7 @@
        ;;swift             ; who asked for emoji variables?
        ;;terra             ; Earth and Moon in alignment for performance.
        (web +lsp +tree-sitter)               ; the tubes
-       ;;yaml              ; JSON, but readable
+       (yaml +tree-sitter)              ; JSON, but readable
        ;;zig               ; C, but simpler
 
        :email
